982420d3d385de4942ea0e98c240ffa0e6eabf13
Matías <matias.agustin.sosa.04@gmail.com>
2026-04-28 18:06:58 -0300

    Codigo ejs

---
+++ servers.js
@@ -0,0 +1,20 @@
+// Inicializar el proyecto (package.json) -- npm init -y
+// Instalar Express -- npm install express
+// Instalar EJS -- npm install ejs
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// CONFIGURACIÓN DE EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get("/", function(rec, res){
+    res.render("index");
+})
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
