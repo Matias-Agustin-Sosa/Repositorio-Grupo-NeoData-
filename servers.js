62fb50ee6c61f299f6c7ff9f5517ef81d3dbb30c
Matías <matias.agustin.sosa.04@gmail.com>
2026-05-05 19:49:01 -0300

    Cambio_de_front

--- servers.js
+++ servers.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // USAR LA CARPETA IMAGEN DE MANERA MAS CORRECTA
 app.use(express.static('Image'));
+app.use(express.json());  
 
 app.get("/", function(rec, res){
     res.render("index");
@@ -21,3 +22,17 @@ app.get("/", function(rec, res){
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
+
+// Simulamos una base de datos en memoria 
+const productosDB = [ 
+{ id: 1, nombre: "Teclado Mecánico", precio: 50000, img: 
+"[https://via.placeholder.com/150](https://via.placeholder.com/150)", descuento: true }, 
+{ id: 2, nombre: "Mouse Gamer", precio: 25000, img: 
+"[https://via.placeholder.com/150](https://via.placeholder.com/150)", descuento: false }, 
+{ id: 3, nombre: "Monitor 24", precio: 180000, img: 
+"[https://via.placeholder.com/150](https://via.placeholder.com/150)", descuento: true } 
+]; 
+app.get('/api/productos', (req, res) => { 
+res.json(productosDB); // Enviamos el arreglo convertido a JSON 
+}); 
