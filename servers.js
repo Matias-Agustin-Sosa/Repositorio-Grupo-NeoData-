b475e8a38c68f62ed9f5b8faf850e6c5c9d9a731
Matías <matias.agustin.sosa.04@gmail.com>
2026-05-25 23:59:06 -0300

    Se conecta el front y back

--- servers.js
+++ servers.js
@@ -1,6 +1,5 @@
-// Inicializar el proyecto (package.json) -- npm init -y
-// Instalar Express -- npm install express
-// Instalar EJS -- npm install ejs
+// Inicializar el proyecto: npm init -y
+// Instalar dependencias: npm install express ejs
 
 const express = require('express');
 const path = require('path');
@@ -14,10 +13,10 @@ app.set('views', path.join(__dirname, 'views'));
 // CONFIGURACIÓN DE ARCHIVOS ESTÁTICOS
 app.use(express.static('Image'));
 
-// MIDDLEWARE: Permite que el servidor entienda los datos en formato JSON en los POST
+// MIDDLEWARE: Permite que Express entienda el "body" en formato JSON [cite: 33, 34]
 app.use(express.json());  
 
-// Base de datos simulada en memoria (Disponible para todas las rutas)
+// Base de datos simulada en memoria [cite: 37]
 const listaProductos = [
     { nombre: "Smartwatch", precio: 125000, stock: 12, descuento: "5%", imagen: "/Producto.png"},
     { nombre: "Celular", precio: 350000, stock: 8, descuento: "2%", imagen: "/Producto.png"},
@@ -28,25 +27,23 @@ const listaProductos = [
 // ==========================================
 // 1. RUTA PARA NAVEGAR (FRONTEND)
 // ==========================================
-// Cuando el usuario entra a http://localhost:3000/ ve la interfaz hecha en EJS
+// Cuando entrás a http://localhost:3000/ se renderiza la interfaz principal
 app.get('/', (req, res) => {
     res.render('index', { productos: listaProductos }); 
 });
 
 // ==========================================
-// 2. ENDPOINT DE LA API (BACKEND - POST)
+// 2. ENDPOINT DE LA API (BACKEND - POST) [cite: 19]
 // ==========================================
-// Aquí es donde el botón "Iniciar Pago" del Modal enviará el carrito mediante Fetch
+// Aquí es donde el botón "Iniciar Pago" enviará el carrito usando Fetch [cite: 54, 121]
 app.post('/api/checkout', (req, res) => {
-    const carritoRecibido = req.body; // Aquí viene la info desde el Frontend
+    const carritoRecibido = req.body;
     
-    console.log("¡NUEVA COMPRA RECIBIDA EN EL SERVIDOR!");
-    console.log(carritoRecibido); // Verás el desglose en la consola de VS Code
+    console.log("¡NUEVA COMPRA RECIBIDA EN EL SERVIDOR!"); // Limpio sin el [cite]
+    console.log(carritoRecibido);
     
-    // Respondemos al cliente con un objeto JSON
     res.json({ mensaje: "Ticket generado con éxito, gracias por su compra" });
 });
-
 
 // INICIAR SERVIDOR
 app.listen(port, () => {
